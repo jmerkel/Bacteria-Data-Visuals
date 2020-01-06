5d8a1eeaa4685127128e1d090d35580e4ddc48de
Joseph Merkel <joseph_merkel@icloud.com>
2020-01-05 17:22:27 -0800

    Initialize complete and Gauge complete

--- plots.js
+++ plots.js
@@ -1,6 +1,7 @@
 function init() {
   var selector = d3.select("#selDataset");
 
+  //Populate Test Subject ID Number
   d3.json("samples.json").then((data) => {
     console.log(data);
     var sampleNames = data.names;
@@ -10,22 +11,20 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
+    
+    // INITIALIZE INFORMATION:
+    // Metadata
+    // Bar Chart
+    // Gauge
+    // Bubble Chart
+
+    buildMetaData(data.names[0]);
+    buildBarChart(data.names[0]);
+    buildGauge(data.names[0]);
+    
+
+    //Initialize Bubble
   })
-  
-  //Initialize Charts//
-  //Initialize Bar
-  barData = [{
-    type: "bar",
-    orientation: "h",
-    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    y: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    text: ["Hello1", "Hello2","Hello3","Hello4","Hello5","Hello6","Hello7","Hello8","Hello9","Hello10",]
-  }];
-  Plotly.newPlot("bar", barData);
-
-  //Initialize Gauge
-
-  //Initialize Bubble
 }
 
 init();
@@ -33,7 +32,7 @@ init();
 function optionChanged(newSample) {
   buildMetaData(newSample);
   buildBarChart(newSample);
-  //buildGauge(newSample);
+  buildGauge(newSample);
   //buildBubbleChart(newSample);
 }
 
@@ -61,93 +60,79 @@ function buildBarChart (sample) {
     var values = result.sample_values.slice(0,10);
     var labels = result.otu_labels.slice(0,10);
     var idNum = result.otu_ids.slice(0,10);
-    //var topSevenCitiesPop = topSevenCities.map(city => parseInt(city.population));
     var idString = idNum.map(id => "OTU_" + id.toString());
-    console.log(values);
-    console.log(labels);
-    console.log(idNum);
-    console.log(idString);
 
     var trace = [{
       type: "bar",
       orientation: "h",
+
       x: values,
       y: idString,
       text: labels
     }];
 
     var layout = {
-      yaxis: {
-        autorange: "reversed"
-      }
+      title: { text: "Top 10 Bacterium Found" },
+      yaxis: { autorange: "reversed" }
     };
 
     Plotly.newPlot("bar", trace, layout);
-    
-    /*
-    d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-    function updatePlotly() {
-      var dropdownMenu = d3.select("#dropdownMenu");
-      var dataset = dropdownMenu.property("value");
-    
-      var xData = [1, 2, 3, 4, 5];
-      var yData = [];
-    
-      if (dataset === 'dataset1') {
-        yData = [1, 2, 4, 8, 16];
-      };
-    
-      if (dataset === 'dataset2') {
-        yData = [1, 10, 100, 1000, 10000];
-      };
-    
-      var trace = {
-        x: [xData],
-        y: [yData],
-      };
-      Plotly.restyle("plot", trace);
-    };
-    */
-
-
-
-    //var sortedOTU = result.sample_values.sort((a,b) => a.sample_values - b.sample_values);
-    //console.log(sortedOTU);
-
-    //var sortedCities = cityGrowths.sort((a,b) => 
-    //a.Increase_from_2016 - b.Increase_from_2016).reverse();
-
-    //x: otu_ids
-    //y: Sample Values
-    //Hover Text = otu_labels
-
-    //var topFiveCities = sortedCities.slice(0,5);
-    //var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    //var result = resultArray[0];
-    //var PANEL = d3.select("#bar");
-
-    //PANEL.html("");
-    //console.log(Object.entries(result))
-    //var demographicInfo = Object.entries(result);
-    //demographicInfo.forEach(item => PANEL.append("h6").text(item[0].toUpperCase() + ": " + item[1]));
   });
 }
 
-/*
-function buildGauge() {
+
+function buildGauge(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-    var PANEL = d3.select("#gauge");
-
-    PANEL.html("");
-    console.log(Object.entries(result))
-    var demographicInfo = Object.entries(result);
-    demographicInfo.forEach(item => PANEL.append("h6").text(item[0].toUpperCase() + ": " + item[1]));
+    var wFreq = result.wfreq;
+    
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { 
+          text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week"  
+        },
+        value: wFreq,
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { 
+            range: [0, 9],
+            tickmode: "linear" 
+          },
+          bar: {
+            color: "dodgerblue",
+            thickness: ".33",
+            line: {
+              width: 3,
+              color: "black" 
+            },
+          },
+          steps: [
+            { range: [0, 1], color: "#2e3d10" },
+            { range: [1, 2], color: "#4c651a" },
+            { range: [2, 3], color: "#6b8e25" },
+            { range: [3, 4], color: "#89b62f" },
+            { range: [4, 5], color: "#a3d049" },
+            { range: [5, 6], color: "#b7da71" },
+            { range: [6, 7], color: "#cce59a" },
+            { range: [7, 8], color: "#e0efc2" },
+            { range: [8, 9], color: "#f5faeb" }
+          ],
+        }
+      }
+    ];
+    var gaugeLayout = { 
+      margin: { t: 0, b: 0 },
+    };
+    Plotly.newPlot('gauge', data, gaugeLayout);
   });
 }
 
+
+/*
 function buildBubbleChart() {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
